2c6d055ce608bcb077d7131986920a47a5f1cfdc
HemaSundarPenugonda <hemasundar@xebia.com>
2015-06-18 17:20:26 +0530

    Test5

--- src/main/java/com/Prowareness/App.java
+++ src/main/java/com/Prowareness/App.java
@@ -5,11 +5,13 @@ package com.Prowareness;
  *
  */
 public class App {
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		System.out.println("XYZ");
+    public static void main(String[] args) {
+	System.out.println("3rd Test");
+	System.out.println("Hello World!");
+	System.out.println("XYZ");
 
-		System.out.println("ABC");
-		System.out.println("1");
-	}
+	System.out.println("ABC");
+	System.out.println("1");
+	System.out.println("3rd Test");
+    }
 }
